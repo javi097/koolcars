b77705837f9bbdbf9c7044cb0cfee2526e96f2e1
JaviGarcia <55480199+javi097@users.noreply.github.com>
2021-02-28 12:42:36 +0100

    Final Segui 2

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,6 @@ Route::resource('carrocerias', CarroceriaController::class);
 Route::resource('marcas', MarcaController::class);
 Route::resource('coches', CocheController::class);
 
-Route::get('carrocerias/{carroceria}', [App\Http\Controllers\MarcaController::class, 'cochesCarroceria'])->name('carrocerias.coche');
-
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
